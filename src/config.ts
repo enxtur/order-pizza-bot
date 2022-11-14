407098e97948bbd7ed43678afb64895dcf804ac2
enxtur <enxtur.mon@gmail.com>
2022-11-14 15:56:53 +0800

    feat: adds page access token

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ export const config = {
   port: process.env.PORT || 3000,
   webhook: {
     verifyToken: process.env.VERIFY_TOKEN || 'verify_token',
+    pageAccessToken: process.env.PAGE_ACCESS_TOKEN || 'page_access_token',
   }
 }
